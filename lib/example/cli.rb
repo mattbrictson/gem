e63618cf2dbc323855a915ffd2631148263f3027
Matt Brictson <matt@mattbrictson.com>
2023-07-23 15:05:28 -0700

    Use Thor as the basis for generated CLI (#148)
    * Hide stack trace unless VERBOSE is set
    * Mention Thor in the README

--- lib/example/cli.rb
+++ lib/example/cli.rb
@@ -1,6 +1,14 @@
+require "thor"
+
 module Example
-  class CLI
-    def call(_argv)
+  class CLI < Thor
+    extend ThorExt::Start
+
+    map %w[-v --version] => "version"
+
+    desc "version", "Display example version", hide: true
+    def version
+      say "example/#{VERSION} #{RUBY_DESCRIPTION}"
     end
   end
 end
