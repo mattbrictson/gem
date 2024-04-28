67b6817399b75652a79cdc3871b61637e3b436e9
Matt Brictson <matt@mattbrictson.com>
2024-04-28 16:09:20 -0700

    Drop support for Ruby 3.0 (EOL) (#175)
    * Drop support for Ruby 3.0 (EOL)
    
    * Adopt Ruby 3.1+ Array#intersect?
    
    * Adopt Ruby 3.1+ implicit hash syntax

--- lib/example/thor_ext.rb
+++ lib/example/thor_ext.rb
@@ -46,7 +46,7 @@ module Example
 
         if given_args.first == "help"
           retry_with_args = ["help"] if given_args.length > 1
-        elsif (e.unknown & %w[-h --help]).any?
+        elsif e.unknown.intersect?(%w[-h --help])
           retry_with_args = ["help", (given_args - e.unknown).first]
         end
         raise unless retry_with_args.any?
